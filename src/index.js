2b3251cdf1d591a04eeee2d8090ce2fb212d209c
sarathdas <sarathharidas18@gmail.com>
2025-01-07 16:59:58 +0800

    asd

--- src/index.js
+++ src/index.js
@@ -58,11 +58,9 @@ function App() {
 }
 
 function Pizza({ pizzaObj }) {
-  //   if (pizzaObj?.soldOut) return null;
-
   return (
     <li className={`pizza ${pizzaObj?.soldOut ? "sold-out" : ""}`}>
-      <img src={pizzaObj?.photoName} alt="{pizzaObj?.name}"></img>
+      <img src={pizzaObj?.photoName} alt={pizzaObj?.name} />
       <div>
         <h3>{pizzaObj?.name}</h3>
         <p>{pizzaObj?.ingredients}</p>
